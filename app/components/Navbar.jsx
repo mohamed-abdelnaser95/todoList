e684bc0a0a8cfeedac349ea3fbb0ceaded94e995
mohamed-abdelnaser95 <mohamed.abdelnaser159@gmail.com>
2024-11-12 18:04:22 +0200

    import the project on github

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+// features
+import Logo from './todo-list.png'
+const Navbar = () => {
+      return (
+            <nav>
+                  <Image 
+                        src={Logo}
+                        alt='ToDo List'
+                        width={40}
+                  />
+                  <Link href='/'>Dashboard</Link>
+                  <Link href='/tickets'>Tickets</Link>
+            </nav>
+      )
+}
+
+export default Navbar
